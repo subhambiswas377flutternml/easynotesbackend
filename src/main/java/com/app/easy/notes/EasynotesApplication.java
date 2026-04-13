072b2183e38c47072aa0b74108f421a73e758c0c
subhambiswas <subham.biswas@innofied.com>
2026-04-13 18:54:03 +0530

    first commit

---
+++ src/main/java/com/app/easy/notes/EasynotesApplication.java
@@ -0,0 +1,13 @@
+package com.app.easy.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasynotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasynotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/easy/notes/EasynotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.easy.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasynotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
